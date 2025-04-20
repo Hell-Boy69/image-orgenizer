a89c40078c982273eeae56cbf80342ba1eff02f3
Hellboy <durjoyprince99@gmail.com>
2025-04-20 23:59:10 +0400

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  pickImages: () => ipcRenderer.invoke('pick-images'),
-  saveImages: (groupName, imagePaths) => ipcRenderer.invoke('save-images', { groupName, imagePaths }),
-  loadImages: (groupName) => ipcRenderer.invoke('load-images', groupName)
-});
